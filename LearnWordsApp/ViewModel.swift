b7294c694bd57ed91e3a11a06f3980f82b16b7a4
Stas <staszhingel@icloud.com>
2021-10-03 20:56:40 +0300

    coredata

--- LearnWordsApp/ViewModel.swift
+++ LearnWordsApp/ViewModel.swift
@@ -6,8 +6,15 @@
 //
 
 import SwiftUI
+import CoreData
 
 class ViewModel: ObservableObject {
+    @Published var updateItem: Task!
+    @Published var word = ""
+    @Published var translatedWord = ""
+    @Published var offset: Float = 0
+    @Published var matchUp = false
+    
     var cards = [Word(word: "First", translatedWord: "Первый", offset: 0),
                  Word(word: "Second", translatedWord: "Второй", offset: 0),
                  Word(word: "Third", translatedWord: "Третий", offset: 0),
@@ -23,4 +30,72 @@ class ViewModel: ObservableObject {
         let randomValue = Int.random(in: 0..<cards.count)
         return randomValue
     }
+    
+    
+    
+    func writeData(context: NSManagedObjectContext) {
+        
+        //updating
+        if updateItem != nil {
+            updateItem.matchUp = matchUp
+            updateItem.word = word
+            updateItem.translatedWord = translatedWord
+            updateItem.offset = offset
+            try! context.save()
+            updateItem = nil
+    //        isNewData.toggle()
+        }
+        
+        else {
+            let newTask = Task(context: context)
+            newTask.matchUp = matchUp
+            newTask.word = word
+            newTask.translatedWord = translatedWord
+            newTask.offset = offset
+            
+            do {
+                try context.save()
+//                title = ""
+//                toDo = "Edit text"
+            }
+            catch {
+                print(error.localizedDescription)
+            }
+        }
+    }
+    
+//    func writeCheck(context: NSManagedObjectContext) {
+//        
+//        //updating
+//        if updateItem != nil {
+//            updateItem.check = check
+//            try! context.save()
+//            updateItem = nil
+//            
+//        }
+//        
+//    }
+//   func EditCheck(item: Task) {
+//        updateItem = item
+//        check = item.check
+//
+//
+//    }
+//    func isPressed(item: Task) {
+//        updateItem = item
+//        isPressed = item.isPressed
+//
+//    }
+//
+//
+//    
+    func delete(context: NSManagedObjectContext, item: Task)  {
+        context.delete(item)
+        try! context.save()
+        
+    }
+
+
+
+
 }
