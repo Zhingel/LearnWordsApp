603b022ded5b30acc86dd9d411330d6eeb07f21f
Stas <staszhingel@icloud.com>
2021-10-03 11:43:16 +0300

    Initial Commit

---
+++ LearnWordsApp/LearnWordsAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearnWordsAppApp.swift
+//  LearnWordsApp
+//
+//  Created by Стас Жингель on 03.10.2021.
+//
+
+import SwiftUI
+
+@main
+struct LearnWordsAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
