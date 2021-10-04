35c6325416e0825f96fc4f0307a8638d0dc931cc
Stas <staszhingel@icloud.com>
2021-10-04 04:28:04 +0300

    saveNew Card

--- LearnWordsApp/Views/AllCardsView.swift
+++ LearnWordsApp/Views/AllCardsView.swift
@@ -8,7 +8,11 @@
 import SwiftUI
 
 struct AllCardsView: View {
+    @State var isNewCard = false
+    @State var text = ""
     @StateObject var viewModel = ViewModel()
+    @Environment(\.managedObjectContext) var context
+    @FetchRequest(entity: Task.entity(), sortDescriptors: [NSSortDescriptor(key: "word", ascending: true)], animation: .spring()) var words: FetchedResults<Task>
     var columns: [GridItem] =
             Array(repeating: .init(.flexible()), count: 2)
     var body: some View {
@@ -16,36 +20,68 @@ struct AllCardsView: View {
             ZStack {
                 ScrollView {
                     LazyVGrid(columns: columns){
-                        ForEach(viewModel.cards) { card in
+                        ForEach(words) { card in
                             ZStack {
                                 RoundedRectangle(cornerRadius: 10)
                                     .frame(width: 150, height: 150)
                                     .foregroundColor(.gray)
                                     .shadow(color: .gray, radius: 1, x: 1, y: -1)
                                 VStack {
-                                    Text(card.word)
+                                    Text(card.word ?? "")
                                         .padding(3)
-                                    Text(card.translatedWord)
+                                    Text(card.translatedWord ?? "")
                                         .padding(3)
                                 }
                             }
                         }
                     }
                 }
-                RoundedRectangle(cornerRadius: 10)
+                .blur(radius: isNewCard ? 6 : 0)
+                .opacity(isNewCard ? 0.6 : 1)
+                if isNewCard {
+                    ZStack {
+                        RoundedRectangle(cornerRadius: 10)
+                            .foregroundColor(.gray)
+                            .shadow(color: .black, radius: 4, x: 3, y: -5)
+                        VStack(spacing: 20) {
+                            Text("Новое слово")
+                                .bold()
+                                .font(.title)
+                                .padding(.vertical, 30)
+                            ZStack {
+                                TextField("на русском", text: $viewModel.word)
+                                    .padding(.horizontal)
+                                RoundedRectangle(cornerRadius: 5)
+                                    .stroke(lineWidth: 1)
+                                    .opacity(0.5)
+                            }
+                            .frame(width: 200, height: 40, alignment: .center)
+                            ZStack {
+                                TextField("на английском", text: $viewModel.translatedWord)
+                                    .padding(.horizontal)
+                                RoundedRectangle(cornerRadius: 5)
+                                    .stroke(lineWidth: 1)
+                                    .opacity(0.5)
+                            }
+                            .frame(width: 200, height: 40, alignment: .center)
+                            Button("Save") {
+                                viewModel.writeData(context: context)
+                            }
+                            Spacer()
+                        }
+                    }
                     .frame(width: 300, height: 300)
-                    .foregroundColor(.gray)
-                    .shadow(color: .black, radius: 4, x: 3, y: -5)
+                }
             }
                     .toolbar {
                         Button(action: {
-                            
+                            isNewCard.toggle()
                         },
                                label: {
                             Image(systemName: "plus")
                         })
                             }
-                    .navigationTitle("Коллкция карточек")
+                    .navigationTitle("Коллекция карточек")
                 .navigationBarTitleDisplayMode(.inline)
         }
     }
